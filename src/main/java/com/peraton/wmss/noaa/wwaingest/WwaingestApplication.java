2dbc110e4656eca3dcc9e035d6dd615b5ba4de83
slush62 <steve@lushfamily.com>
2020-05-13 15:47:35 -0500

    Initial commit of spring.io project

---
+++ src/main/java/com/peraton/wmss/noaa/wwaingest/WwaingestApplication.java
@@ -0,0 +1,13 @@
+package com.peraton.wmss.noaa.wwaingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwaingestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwaingestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peraton/wmss/noaa/wwaingest/WwaingestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peraton.wmss.noaa.wwaingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwaingestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
